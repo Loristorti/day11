a4e527d5e2371b8878c2cc256e1e99b3aacb9f84
Loristorti <loris.torti@epitech.digital>
2024-10-14 21:11:35 +0200

    Create ex_04.js

---
+++ ex_04.js
@@ -0,0 +1,11 @@
+unction initializeMachine(machineName, timeRequired) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(`${machineName} initialized`);
+        }, timeRequired * 1000);
+    });
+}
+
+initializeMachine("Computer", 3).then((value) => {
+    console.log(value);
+});
